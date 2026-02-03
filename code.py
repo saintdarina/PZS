d22807483efcf449a8125fa23dd81a0f8eeadd51
saintdarina <darina2004722@gmail.com>
2026-02-03 22:51:33 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,32 @@
+'''
+Druhá seminární práce.
+
+Postup:
+
+Máme hlasové signály pacientů a jejich diagnozy.
+
+Naším úkolem je:
+
+1. načíst signál
+2. získat z něj vhodné příznaky
+3. podle nich signály automaticky rozdělit
+4. porovnat výsledek s realitou (anotacemi)
+
+Načtení dat bude probíhat pomocí WFDB. Výstupem bude pole vzorků signálu, vzorkovácí frekvence, diagnoza / třída.
+
+Předzpracování signálu. Odstranění DC složky, normalizace amplitudy, případně okenování.
+
+Ektrakce příznaků. 
+Časová oblast: RMS hodnota, energie signálu, variance, zero-crossing fate, jitter.
+Frekvenční oblast: FFT spektrum, spektrální centroid, šířka pasma, dominantní frekvence, poměr energie v pásmech.
+Kepstrální analýza: log(FFT), IFFT -> kepstrum, hledaní periodicity. 
+
+Klasifikace.
+Prahování, vzdálenost středu třídy, k-means, jednoduchý kNN, PCA -> vizualizace, oddělení: normální x patologický,
+typy patologií.
+
+Vyhodnocení úspěšnosti. Porovnání našeho výsledku a anotací jako tabulka.
+
+Grafické výstupy: signál v čase, FFT spektrum, kepstrum, PCA scatter plot, rozhodovací hranice.
+'''
 
